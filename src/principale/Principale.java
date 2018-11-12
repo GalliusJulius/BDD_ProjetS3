6f66273e16167af77f23ffcaefa6c331fed549f9
Victor Moreliere <victormoreliere@gmail.com>
2018-11-12 17:13:47 +0000

    ajout trigger 1 mode console

---
+++ src/principale/Principale.java
@@ -0,0 +1,24 @@
+package Principale;
+import java.util.Scanner;
+
+import graphique.Fenetre;
+import javafx.application.Application;
+
+/**
+ * Classe principale de l'application.
+ */
+public class Principale {
+	
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Vous allez choisir entre le mode graphique ou le mode console, pour le mode graphique tapez 1 sinon tapez n'importe quoi d'autre!");
+		String rep = sc.next();
+		if(rep.equals("1")) {
+			Application.launch(Fenetre.class, args);
+		}
+		else {
+			ModeConsole mod = new ModeConsole();
+		}
+	}
+
+}
